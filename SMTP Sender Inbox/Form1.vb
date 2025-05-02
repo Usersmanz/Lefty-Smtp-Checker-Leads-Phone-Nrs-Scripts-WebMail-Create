ab39dc49baedcfc9108cbf61cb04afe2cc295ba2
Decipher0956 <freebie7729@outlook.com>
2025-05-02 13:44:37 +0400

    Form1.vb

---
+++ SMTP Sender Inbox/Form1.vb
@@ -0,0 +1,11 @@
+﻿'Coloer
+'#533071
+Public Class Form1
+    Private Sub Guna2HtmlLabel1_Click(sender As Object, e As EventArgs) Handles Guna2HtmlLabel1.Click
+
+    End Sub
+
+    Private Sub Guna2HtmlLabel3_Click(sender As Object, e As EventArgs)
+        Form2.Show()
+    End Sub
+End Class
